2a031d50604285dddef4970457724fea733d5c3a
Tafita Hery <tafitahery@gmail.com>
2022-09-19 20:22:07 +0300

    Using Material UI Icons with react

--- src/component/topbar/Topbar.jsx
+++ src/component/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import './topbar.scss';
+import { Person, Mail } from '@mui/icons-material';
 
 export default function Topbar() {
   return (
@@ -8,6 +9,14 @@ export default function Topbar() {
           <a href="#intro" className="logo">
             genius.
           </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+261 34 52 675 80</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>tafitahery@gmail.com</span>
+          </div>
         </div>
         <div className="right"></div>
       </div>
